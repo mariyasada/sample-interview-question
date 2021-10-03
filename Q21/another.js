e084c728b7efcb1b4c25abbfce07a7e5c8d82480
mariyasada <mariyasada2406@gmail.com>
2021-10-03 17:06:39 +0530

    adding js

---
+++ Q21/another.js
@@ -0,0 +1,44 @@
+// this is for practicing with array. find a largest number
+const checkBtn = document.querySelector("#check");
+const outputEl = document.querySelector("#output");
+const arrayList=[20,30,40,60,90,1000,200,55,2222];
+
+
+checkBtn.addEventListener("click",()=>{
+
+    console.log("clicked");
+    var element = arrayList[0];
+    var highestelement ="";
+    for(let i=0; i< arrayList.length;i++)
+    {
+        console.log(element);
+        if(element < arrayList[i])
+        {
+            console.log(arrayList[i])
+
+               highestelement = arrayList[i];
+               outputEl.innerText=" highest element is  " + highestelement;
+        }
+    }
+
+});
+
+function smallest()
+{
+    const arrayList=[20,30,40,60,90,1000,200,55,2222];
+    var element =arrayList[arrayList.length-1];
+    console.log(element);
+    var smallestnumber= "";
+
+    for( let i=0; i<arrayList.length;i++)
+    {
+        if(element > arrayList[i])
+        {
+            console.log(element);
+            smallestnumber=arrayList[i];
+            outputEl.innerText=" smallest element is  " + smallestnumber;
+        }
+    }
+}
+
+smallest();
