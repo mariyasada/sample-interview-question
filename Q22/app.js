7d70022a45b3fad3c70be5b9aca00f489503a5b4
mariyasada <mariyasada2406@gmail.com>
2021-10-04 15:54:36 +0530

    add one button

--- Q22/app.js
+++ Q22/app.js
@@ -2,6 +2,7 @@
 const twoPlusBtn = document.querySelector("#two");
 const twoMinusBtn = document.querySelector("#minustwo");
 const input = document.querySelector("#inputtext");
+const plusFive = document.querySelector("#plusfive");
 var fontsize=16;
 
 twoPlusBtn.addEventListener("click",()=>{
@@ -15,4 +16,9 @@ twoMinusBtn.addEventListener("click",()=>{
     input.style.fontSize = (fontsize-2) +'px';
     console.log(input.style.fontSize);
     input.style.color ="red";
+});
+
+plusFive.addEventListener("click",()=>{
+    input.style.fontSize = (fontsize + 5) + 'px';
+    console.log(input.style.fontSize);
 })
