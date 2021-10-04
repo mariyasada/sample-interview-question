59b1cd654f8961888ca97bad71c4c064e917947b
mariyasada <mariyasada2406@gmail.com>
2021-10-04 15:51:40 +0530

    solving one more question

---
+++ Q22/app.js
@@ -0,0 +1,18 @@
+
+const twoPlusBtn = document.querySelector("#two");
+const twoMinusBtn = document.querySelector("#minustwo");
+const input = document.querySelector("#inputtext");
+var fontsize=16;
+
+twoPlusBtn.addEventListener("click",()=>{
+    console.log("clicked");
+    input.style.fontSize = (fontsize + 2) + 'px';
+    console.log(input.style.fontSize);
+    input.style.color ="green";
+
+});
+twoMinusBtn.addEventListener("click",()=>{
+    input.style.fontSize = (fontsize-2) +'px';
+    console.log(input.style.fontSize);
+    input.style.color ="red";
+})
