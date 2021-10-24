f8d52b7669c6ded16d257f33d1a7c60db80cd1ac
mariyasada <mariyasada2406@gmail.com>
2021-10-24 13:19:38 +0530

    add one more file

---
+++ Q26/app.js
@@ -0,0 +1,5 @@
+const btnZNMD = document.querySelector("#btn-znmd");
+const btnYHJD = document.querySelector("#btn-yjhd");
+const outputEl=document.querySelector("#output");
+
+const url ="https://mock-practice.prakhar10v.repl.co/bollywood?name=";
