2c7d8d9997cd28fb3b73fe30a31864c4cfcf2ab9
mariyasada <mariyasada2406@gmail.com>
2021-10-24 13:45:20 +0530

    docs: adding some code

--- Q26/app.js
+++ Q26/app.js
@@ -3,3 +3,28 @@ const btnYHJD = document.querySelector("#btn-yjhd");
 const outputEl=document.querySelector("#output");
 
 const url ="https://mock-practice.prakhar10v.repl.co/bollywood?name=";
+
+btnYHJD.addEventListener("click",()=> {
+
+    var btnValue = btnYHJD.innerText;
+    console.log(btnValue);
+    callFilmValue(btnValue);
+});
+
+btnZNMD.addEventListener("click",()=> {
+
+    var btnValue = btnZNMD.innerText;
+    console.log(btnValue);
+    callFilmValue(btnValue);
+});
+
+function callFilmValue(filmname)
+{
+    fetch(url + filmname)
+    .then(response => response.json())
+    .then(json => 
+        {
+            console.log(json)
+            outputEl.innerText= json.message;
+        })
+}
